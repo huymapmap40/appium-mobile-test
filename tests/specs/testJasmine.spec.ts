2bcb5772d34acd410584c57fd818f2eb6dca0645
Huy Nguyen <37060736+huymapmap40@users.noreply.github.com>
2021-10-15 23:29:20 +0700

    Add files via upload
    add files script

---
+++ tests/specs/testJasmine.spec.ts
@@ -0,0 +1,26 @@
+const path = require('path');
+const wd = require('webdriverio');
+
+describe("Test jasmine", () => {
+    it("First test case", async () => {
+
+        let appiumCapabilities = {
+                'platformName': 'Android',
+                'platformVersion': '11',
+                'deviceName': 'Pixel4',
+                'automationName': 'UiAutomator2',
+                'app': path.join(process.cwd(), './apps/Android-NativeDemoApp-0.4.0.apk')
+        }
+
+        let opts = {
+            path: '/wd/hub',
+            port: 4723,
+            capabilities: appiumCapabilities
+        }
+        
+        let driverSession = await wd.remote(opts);
+        console.log('==========>', driverSession);
+
+        expect(1).toBe(1);
+    })
+});
